9e717a7162e84d82cb09b5e0d062b9b48ce9620b
Jimmy Tsai <79801431+Jimmy-frog@users.noreply.github.com>
2021-10-19 22:01:48 +0800

    Add files via upload

---
+++ 自動化pywright.py
@@ -0,0 +1,24 @@
+from playwright.sync_api import sync_playwright
+
+
+# 網頁截圖
+with sync_playwright() as p:
+    #brower = p.chromium.launch()
+    brower = p.chromium.launch(headless=False)
+    
+    #訂製畫面大小
+    context = brower.new_context(
+        viewport={"width":750, "height":1334}
+    )
+    
+    
+    
+    
+    page = brower.new_page()
+    page.goto("https://www.google.com")
+    
+    # 截圖
+    #page.screenshot(path="pyout1.png")
+    brower.close()
+
+
